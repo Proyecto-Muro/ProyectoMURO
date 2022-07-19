394f3a3cf18adf56f3fbfb4f2b2a8c5511b4be89
Full Name <dcr31415@gmail.com>
2022-07-19 01:49:19 -0500

    Major website style changes. Added some smaller features.

--- PagesGenerator.py
+++ PagesGenerator.py
@@ -8,17 +8,30 @@ import os
 # Generate text for all high-level pages, and all folder pages
 
 
-def replace_all(text, dic):
+# This is the main function used for the generator. It replaces 
+# text where needed in a template. 
+def replace_all(text, dic): 
     for i, j in dic.items():
         text = text.replace(str(i), str(j))
     return text
 
 
+# The fourth* entry in the dict is for years that need to be omitted.
+# The third entry is the last year the contest took place
+contestinfo = [
+    ["IMO", 1959, 2022, [1980]],
+    ["OMCC", 1999, 2021, []],
+    ["OMM", 1987, 2021, []],
+    ["EGMO", 2012, 2022, []],
+    ["OIM", 1985, 2021, [1986]],
+]
+
 # define all problems from /concursos/contest/[year]/enunciados
 
+def htmlproblems(contestname, year, ismax=False, ismin=False):
 
-def htmlproblems(contestname, year):
-    # define the problems, set as empty if problem does not exist
+    # Declare the problems, will be set as empty if problem does not exist
+
     ProblemsList = []
     Problem = ""
     ProblemPosition = "1"
@@ -26,8 +39,18 @@ def htmlproblems(contestname, year):
         "[Enunciadoproblema]": str(Problem),
         "[concurso]": str(contestname),
         "[año]": str(year),
-        "[numproblema]": str(ProblemPosition)
+        "[numproblema]": str(ProblemPosition),
+        "[añoantes]": r'<a href="./../' + str(year - 1) + r'">◄</a>',
+        "[añodespues]": r'<a href="./../' + str(year + 1) + r'">►</a>'
     }
+
+    # This is to remove the arrows for first and last years.
+    if ismax:
+        dic["[añodespues]"]="" 
+    if ismin:
+        dic["[añoantes]"]=""
+
+
     Plist = os.listdir("./concursos/%s/%s/enunciados" % (contestname, year))
     Plist.sort()
     for i in Plist:
@@ -74,21 +97,28 @@ def htmlproblems(contestname, year):
         open("generator/formats/solution-page.txt", "r").close()
 
 
-def GenerateYearLinks(year_start, year_end):
+# Generates links for each year, to be used in contestindex pages
+
+def GenerateYearLinks(year_start, year_end, exception_list):
     text = ''
     for i in range(year_end, year_start - 1, -1):
-        text += r"<h3><a href=./%s>%s</a></h3>" % (i, i)
+        if i in exception_list: # For contests that did not take place
+            text += r"<h3><a href=./%s>%s</a>*</h3>" % (i, i)
+        else:
+            text += r"<h3><a href=./%s>%s</a></h3>" % (i, i)
     return text
 
 
-def ReloadContestText():
-    from contestinfo import contestList
+
+
+def ReloadContestText(): # Writes contest information into main page, adds links for each year
+
     indexraw = open("generator/formats/contest-index.txt", "r").read()  # this is the format
     # We need a text and format for each contest.
-    for i in contestList:
-        yearRefs = GenerateYearLinks(i[1], i[2])
+    for i in contestinfo:
+        yearRefs = GenerateYearLinks(i[1], i[2], i[3]) # Adds each link 
         concurso = i[0]
-        contestText = open("generator/texts/%s.txt" % concurso, "r").read()
+        contestText = open("generator/texts/%s.txt" % concurso, "r").read() 
         dic = {
             "[concurso]": concurso,
             "[yearlinks]": yearRefs,  # yearlinks must come from a list from each contest, and generate
@@ -102,7 +132,7 @@ def ReloadContestText():
     open("generator/formats/contest-index.txt").close()
 
 
-def GenerateOthers():
+def GenerateOthers(): # Generates pages from pageslist file
     from pageslist import pages_list
     with open("generator/formats/normal-page.txt", "r") as pagecontentraw:
         for i in pages_list:
@@ -126,20 +156,13 @@ def GenerateOthers():
 
 def ReloadPages():
     # Reload Problem and Solution Pages
-    # The third entry in the dict is for years that need to be omitted.
-    contestinfo = [
-        ["IMO", 1959, [1980]],
-        ["OMCC", 1999, []],
-        ["OMM", 1987, []],
-        ["EGMO", 2012, []],
-        ["OIM", 1985, [1986]],
-    ]
-    for i in contestinfo:
-        for j in range(i[1], 2022):
-            if j not in i[2]:
-                htmlproblems(i[0], j)
+    for contest in contestinfo:
+        for year in range(contest[1], contest[2]+1):
+            if year not in contest[3]: # If the year did take place, generate the page
+                htmlproblems(contest[0], year, year==contest[2], year==contest[1]) # Uses == to check if it is the first or last year.
             else:
                 # Add a page: Contest did not take place said year.
+                
                 pass
 
     # Reload Contest Indexes, including text
