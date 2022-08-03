1d0865046c5a743b7de168883a8c1c219e6c2acd
Full Name <dcr31415@gmail.com>
2022-08-02 23:42:25 -0500

    Added Iberos, created pdf compilation generator, made pure tex problems

---
+++ htmltotex.py
@@ -0,0 +1,35 @@
+import os
+
+def replace_all(text, dic): 
+    for i, j in dic.items():
+        text = str(text).replace(str(i), str(j))
+    return text
+
+replacedict = {
+	"<br>": r" \newline ",
+	"<ul>": r" \begin{itemize} ",
+	"</ul>": r" \end{itemize} ",
+	"<ol>": r" \begin{enumerate} ",
+	"</ol>": r" \end{enumerate} ",
+	"<li>": r" \item ",
+	"</li>": ""
+}
+
+def htmltotex(text):
+	return replace_all(text,replacedict)
+
+#counter=0
+contestlist = ["APMO", "EGMO", "IMO", "ISL", "OIM", "OMCC", "OMM", "RMM"]
+for i in contestlist:
+	for j in os.listdir("concursos/"+i):
+		if j[0]!=".":
+			for k in os.listdir("concursos/%s/%s/enunciados"%(i,j)):
+				if k[0]!=".":
+					fhtml = open("concursos/%s/%s/enunciados/%s"%(i,j,k), "r").read()
+					#if f!="":
+					#	counter+=1
+					ftex = htmltotex(fhtml)
+					testempty = open("concursostex/%s/%s/enunciados/%s"%(i,j,k), "r").read()
+					if testempty =="":
+						with open("concursostex/%s/%s/enunciados/%s"%(i,j,k), "w") as file:
+							file.write(ftex)
